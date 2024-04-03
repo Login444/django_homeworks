0574aec53292e3455978ca3afbde1a4adf39066c
Artemiy <birukovartemij@gmail.com>
2024-04-03 16:21:48 +0300

    3е ДЗ выполнено

--- birukovproject/marketapp/urls.py
+++ birukovproject/marketapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('orders/<int:client_id>/<int:time_diapazone>', views.products_list, name='product_list'),
 ]
--- birukovproject/marketapp/views.py
+++ birukovproject/marketapp/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render, HttpResponse
 from .models import Client, Product, Order
+from datetime import datetime, timedelta
+
 
 # Create your views here.
+
+def products_list(request, client_id: int, time_diapazone: int):
+    client = Client.objects.get(pk=client_id)
+    start_date = datetime.now() - timedelta(days=time_diapazone)
+    orders = Order.objects.filter(client=client, order_date__gte=start_date).prefetch_related('products').order_by(
+        '-order_date')
+    products = set()
+    for order in orders:
+        products |= set(order.products.all())
+    return render(request,
+                  'marketapp/orders_by_client.html',
+                  {'title': "Orders", 'products': products, 'client_name': client.name, 'time': time_diapazone})
